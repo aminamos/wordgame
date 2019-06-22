126b901bf80d210ef2d49c31e22e858b904bf70c
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-22 10:31:23 -0700

    initial commit for word game practice

---
+++ game.rb
@@ -0,0 +1,21 @@
+# Prompt the user to enter a verb, and store what they enter
+# in a variable named "verb".
+
+# Prompt the user to enter a noun, and store what they enter
+# in a variable named "noun".
+
+# Prompt the user to enter an adjective, and store what they
+# enter in a variable named "adjective".
+
+# Prompt the user to enter a second noun, and store what they
+# enter in a variable named "noun2".
+
+# Fill in the #{} marker to include the contents of the
+# "verb" variable in the below string.
+puts "One day, I decided to learn to #{} in Ruby."
+# Use the contents of the "noun" variable in this sentence.
+puts "So I turned on my #{} and logged in to Treehouse."
+# Use the contents of the "adjective" variable in this sentence.
+puts "Their teachers were really #{}."
+# Use the contents of the "noun2" variable in this sentence.
+puts "In no time, I'd learned to program a simple #{}!"
