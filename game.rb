c19f88ce4f4ecfa818ad197168c8d409da5a9fd3
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-22 10:38:53 -0700

    finished game

--- game.rb
+++ game.rb
@@ -1,21 +1,40 @@
 # Prompt the user to enter a verb, and store what they enter
 # in a variable named "verb".
 
+p "Enter a verb:"
+verb = gets.chomp
+
 # Prompt the user to enter a noun, and store what they enter
 # in a variable named "noun".
+
+p "Enter a noun:"
+noun = gets.chomp
 
 # Prompt the user to enter an adjective, and store what they
 # enter in a variable named "adjective".
 
+p "Enter an adjective:"
+adjective = gets.chomp
+
 # Prompt the user to enter a second noun, and store what they
 # enter in a variable named "noun2".
 
+p "Enter another noun:"
+noun2 = gets.chomp
+
 # Fill in the #{} marker to include the contents of the
 # "verb" variable in the below string.
-puts "One day, I decided to learn to #{} in Ruby."
+
+puts "One day, I decided to learn to #{verb} in Ruby."
+
 # Use the contents of the "noun" variable in this sentence.
-puts "So I turned on my #{} and logged in to Treehouse."
+
+puts "So I turned on my #{noun} and logged in to Treehouse."
+
 # Use the contents of the "adjective" variable in this sentence.
-puts "Their teachers were really #{}."
+
+puts "Their teachers were really #{adjective}."
+
 # Use the contents of the "noun2" variable in this sentence.
-puts "In no time, I'd learned to program a simple #{}!"
+
+puts "In no time, I'd learned to program a simple #{noun2}!"
